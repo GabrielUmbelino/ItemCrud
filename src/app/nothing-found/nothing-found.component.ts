fa4ddad3aad5c071f889585ada30ab9e3a8d96bb
Unknown <gabriel.umbelino@hitechnologies.com.br>
2018-07-30 00:52:48 -0300

    more configuration files

---
+++ src/app/nothing-found/nothing-found.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nothing-found',
+  styleUrls: ['./nothing-found.component.css'],
+  templateUrl: './nothing-found.component.html',
+})
+export class NothingFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
