8fca7fd6198438a05ca8d0671a699a5613bdf25a
Unknown <gabriel.umbelino@hitechnologies.com.br>
2018-08-05 17:02:41 -0300

    normalize module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
-import { AccordionModule } from 'primeng/primeng';
-import { PanelModule } from 'primeng/primeng';
-import { ButtonModule } from 'primeng/primeng';
-import { RadioButtonModule } from 'primeng/primeng';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpModule } from '@angular/http';
 
 import { ItemsModule } from './items/items.module';
 
-import { HttpModule } from '@angular/http';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FormsModule,
-    AccordionModule,
-    PanelModule,
-    ButtonModule,
-    RadioButtonModule,
     ItemsModule,
     AppRoutingModule,
     HttpModule
   ],
   providers: [],
+  declarations: [
+    AppComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
